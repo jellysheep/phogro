28bd9839d956688b814a1e98746178495e3742bd
jellysheep <max.mail@dameweb.de>
2014-07-25 12:18:20 +0200

    Added presenter interface files.

---
+++ src/Interfaces/ICategoryPresenter.hpp
@@ -0,0 +1,20 @@
+/**
+ * \file
+ * \authors Max Mertens
+ *
+ * Copyright (c) 2014, Max Mertens. All rights reserved.
+ * This file is licensed under the "BSD 3-Clause License".
+ * Full license text is under the file "LICENSE" provided with this code.
+ */
+
+#if !defined ICATEGORY_PRESENTER_HPP_INCLUDED
+#define ICATEGORY_PRESENTER_HPP_INCLUDED
+
+namespace Interfaces {
+
+class ICategoryPresenter {
+};
+
+} /* namespace Interfaces */
+
+#endif /* !defined ICATEGORY_PRESENTER_HPP_INCLUDED */
