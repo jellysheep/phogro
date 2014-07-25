94e557be2fcfccab194b19e99923cc70db5f1d8c
jellysheep <max.mail@dameweb.de>
2014-07-25 12:28:52 +0200

    Added basic view interface classes.

---
+++ src/Interfaces/IImageView.hpp
@@ -0,0 +1,20 @@
+/**
+ * \file
+ * \authors Max Mertens
+ *
+ * Copyright (c) 2014, Max Mertens. All rights reserved.
+ * This file is licensed under the "BSD 3-Clause License".
+ * Full license text is under the file "LICENSE" provided with this code.
+ */
+
+#if !defined IIMAGE_VIEW_HPP_INCLUDED
+#define IIMAGE_VIEW_HPP_INCLUDED
+
+namespace Interfaces {
+
+class IImageView {
+};
+
+} /* namespace Interfaces */
+
+#endif /* !defined IIMAGE_VIEW_HPP_INCLUDED */
---
+++ src/Interfaces/IProjectView.hpp
@@ -0,0 +1,20 @@
+/**
+ * \file
+ * \authors Max Mertens
+ *
+ * Copyright (c) 2014, Max Mertens. All rights reserved.
+ * This file is licensed under the "BSD 3-Clause License".
+ * Full license text is under the file "LICENSE" provided with this code.
+ */
+
+#if !defined IPROJECT_VIEW_HPP_INCLUDED
+#define IPROJECT_VIEW_HPP_INCLUDED
+
+namespace Interfaces {
+
+class IProjectView {
+};
+
+} /* namespace Interfaces */
+
+#endif /* !defined IPROJECT_VIEW_HPP_INCLUDED */
