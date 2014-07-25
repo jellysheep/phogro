f8759fc578d87f4e88f8aeba4fd4909b5b76b8b0
jellysheep <max.mail@dameweb.de>
2014-07-25 11:37:24 +0200

    Added basic source files.

---
+++ src/Model/Category.hpp
@@ -0,0 +1,21 @@
+/**
+ * \file
+ * \authors Max Mertens
+ *
+ * Copyright (c) 2014, Max Mertens. All rights reserved.
+ * This file is licensed under the "BSD 3-Clause License".
+ * Full license text is under the file "LICENSE" provided with this code.
+ */
+
+#if !defined CATEGORY_HPP_INCLUDED
+#define CATEGORY_HPP_INCLUDED
+
+namespace Model {
+
+class Category {
+
+};
+
+} /* namespace Model */
+
+#endif /* !defined CATEGORY_HPP_INCLUDED */
